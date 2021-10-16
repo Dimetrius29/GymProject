92efd81813a5140f6a8b11157f622b52a51569a9
Dimetrius29 <dimetrius2991@gmail.com>
2021-10-16 13:12:10 +0300

    Service info and readme corrected, primary keys added

--- src/main/resources/db/migration/V1__CreateTables.sql
+++ src/main/resources/db/migration/V1__CreateTables.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS user (
     id int IDENTITY NOT NULL PRIMARY KEY,
     login varchar(50) NOT NULL UNIQUE,
-    password varchar(50) NOT NULL,
+    password varchar(255) NOT NULL,
     name varchar(50) NOT NULL,
     surname varchar(50) NOT NULL,
     phone char(13) UNIQUE
@@ -20,14 +20,17 @@ CREATE TABLE IF NOT EXISTS user_role
     user_id int NOT NULL,
     role_id int NOT NULL,
     FOREIGN KEY (user_id) REFERENCES user (id),
-    FOREIGN KEY (role_id) REFERENCES role (id)
+    FOREIGN KEY (role_id) REFERENCES role (id),
+    PRIMARY KEY (user_id,role_id)
 );
 
 CREATE TABLE IF NOT EXISTS membership (
     user_id int NOT NULL,
     discount int NOT NULL,
     expiration_date date NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES user (id)
+    FOREIGN KEY (user_id) REFERENCES user (id),
+    PRIMARY KEY (user_id)
+
 );
 
 CREATE TABLE IF NOT EXISTS coach (
@@ -39,18 +42,12 @@ CREATE TABLE IF NOT EXISTS coach (
     price_of_activity decimal (4,2) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS schedule (
-    id int NOT NULL,
-    time_interval varchar(13) NOT NULL,
-    PRIMARY KEY (time_interval)
-);
-
 CREATE TABLE IF NOT EXISTS service_info (
     user_id int,
     coach_id int NOT NULL,
     date_info date NOT NULL,
-    time_interval varchar(13) NOT NULL,
+    start_time time NOT NULL,
+    end_time time NOT NULL,
     FOREIGN KEY (user_id) REFERENCES user (id),
-    FOREIGN KEY (coach_id) REFERENCES coach (id),
-    FOREIGN KEY (time_interval) REFERENCES schedule (id)
+    FOREIGN KEY (coach_id) REFERENCES coach (id)
 );
